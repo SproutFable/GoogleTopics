444616283443bb4fadcce5d1d0347546a3a13daf
ehsankolivand <ehsankolivandeh@gmail.com>
2021-05-04 04:31:13 -0700

    Share data between a parent and child fragment with FragmentResult Api

---
+++ fragmentresultapi/src/main/java/com/ehsankolivand/fragmentresultapi/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.ehsankolivand.fragmentresultapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.commit
+import com.ehsankolivand.fragmentresultapi.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var mainBinding: ActivityMainBinding
+    lateinit var fragmentA:FragmentA
+    lateinit var fragmentB: FragmentB
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = mainBinding.root
+        setContentView(view)
+
+        fragmentA = FragmentA.newInstance()
+        fragmentB = FragmentB.newInstance()
+
+        supportFragmentManager.commit {
+            add(R.id.fragment_container,fragmentA)
+            addToBackStack("a")
+            add(R.id.fragment_container,fragmentB)
+            addToBackStack("b")
+
+        }
+    }
+}
