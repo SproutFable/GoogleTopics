52a516b1124d3930af8e76da24ebb77888747bc6
ehsankolivand <ehsankolivandeh@gmail.com>
2021-05-10 02:08:46 -0700

    Services: setup ViewBinding

--- services/src/main/java/com/ehsankolivand/services/MainActivity.kt
+++ services/src/main/java/com/ehsankolivand/services/MainActivity.kt
@@ -2,10 +2,15 @@ package com.ehsankolivand.services
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ehsankolivand.services.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var activityMainBinding: ActivityMainBinding?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = activityMainBinding!!.root
+        setContentView(view)
     }
 }
