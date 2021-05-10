72b0392839ed1806576750ec02f0357d160d9b72
ehsankolivand <ehsankolivandeh@gmail.com>
2021-05-10 06:03:03 -0700

    Intent Services: Make IntentService for play music in background MediaIntentService.kt

--- intentservices/src/main/java/com/ehsankolivand/intentservices/MediaIntentService.kt
+++ intentservices/src/main/java/com/ehsankolivand/intentservices/MediaIntentService.kt
@@ -2,12 +2,47 @@ package com.ehsankolivand.intentservices
 
 import android.app.IntentService
 import android.content.Intent
+import android.media.MediaPlayer
+import android.provider.Settings
+import android.util.Log
+import java.lang.Exception
 
-class MediaIntentService(name: String?) :IntentService(name) {
+class MediaIntentService private constructor(name: String?): IntentService(name) {
+
+    private lateinit var media:MediaPlayer
+    companion object{
+        var INSTANCE:MediaIntentService?=null
+        var isRunning = false
+
+        fun newInstance() = synchronized(this){
+            if (INSTANCE==null)
+                INSTANCE = MediaIntentService("mediaPlayer")
+            INSTANCE
+        }
+        fun stop()
+        {
+            if (isRunning){
+                INSTANCE?.stopSelf()
+                isRunning=false
+            }
 
 
-
+        }
+    }
 
     override fun onHandleIntent(intent: Intent?) {
+        isRunning = true
+        media = MediaPlayer.create(this,Settings.System.DEFAULT_ALARM_ALERT_URI)
+        media.start()
+
     }
+
+    override fun stopService(name: Intent?): Boolean {
+        if (isRunning)
+        {
+            media.stop()
+        }
+        return super.stopService(name)
+    }
+
 }
