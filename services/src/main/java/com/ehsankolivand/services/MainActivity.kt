40132517114dcbecd37a20f3890e6342e7498da4
ehsankolivand <ehsankolivandeh@gmail.com>
2021-05-10 02:13:20 -0700

    Services: add OnClickListener for handle start and stop button

--- services/src/main/java/com/ehsankolivand/services/MainActivity.kt
+++ services/src/main/java/com/ehsankolivand/services/MainActivity.kt
@@ -2,15 +2,34 @@ package com.ehsankolivand.services
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.ehsankolivand.services.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     var activityMainBinding: ActivityMainBinding?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         val view = activityMainBinding!!.root
         setContentView(view)
+
+        activityMainBinding!!.btnStart.setOnClickListener(this)
+        activityMainBinding!!.btnStop.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+
+        when(v)
+        {
+            activityMainBinding!!.btnStart -> {
+
+            }
+            activityMainBinding!!.btnStop->{
+
+            }
+        }
     }
 }
