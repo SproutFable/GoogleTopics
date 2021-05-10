5ba96741d4c92404e68424a02949419b81252e63
ehsankolivand <ehsankolivandeh@gmail.com>
2021-05-10 07:57:50 -0700

    BroadCastReceiver: First Create BroadCastReceiver and then register it for receive AirPlaneMode state and reaction

--- broadcastreceiver/src/main/java/com/ehsankolivand/broadcastreceiver/MainActivity.kt
+++ broadcastreceiver/src/main/java/com/ehsankolivand/broadcastreceiver/MainActivity.kt
@@ -1,17 +1,31 @@
 package com.ehsankolivand.broadcastreceiver
 
+import android.content.BroadcastReceiver
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ehsankolivand.broadcastreceiver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     var activityMainBinding: ActivityMainBinding?=null
+    private lateinit var myBR: MyBroadCastReceiver
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         val view = activityMainBinding!!.root
         setContentView(view)
+        myBR = MyBroadCastReceiver()
+        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
+            registerReceiver(myBR,it)
+        }
 
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(myBR)
     }
 
 
---
+++ broadcastreceiver/src/main/java/com/ehsankolivand/broadcastreceiver/MyBroadCastReceiver.kt
@@ -0,0 +1,13 @@
+package com.ehsankolivand.broadcastreceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyBroadCastReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val state = intent?.getBooleanExtra("state",false) ?:return
+        Toast.makeText(context,"BroadCast Receive",Toast.LENGTH_LONG).show()
+    }
+}
