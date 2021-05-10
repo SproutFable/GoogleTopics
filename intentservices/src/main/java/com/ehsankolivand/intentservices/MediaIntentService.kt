a7823d13ba72f76f6bb1628b418654d57a3661a9
ehsankolivand <ehsankolivandeh@gmail.com>
2021-05-10 03:42:00 -0700

    Intent Services: Add MediaIntentService.kt and extend IntentService Class Note : IntentService in deprecated and you can use JobIntentService Or WorkManager

---
+++ intentservices/src/main/java/com/ehsankolivand/intentservices/MediaIntentService.kt
@@ -0,0 +1,13 @@
+package com.ehsankolivand.intentservices
+
+import android.app.IntentService
+import android.content.Intent
+
+class MediaIntentService(name: String?) :IntentService(name) {
+
+
+
+
+    override fun onHandleIntent(intent: Intent?) {
+    }
+}
