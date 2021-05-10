909721ab4cfa6a44ecb4c7394974d3049e0d29c3
ehsankolivand <ehsankolivandeh@gmail.com>
2021-05-10 03:38:15 -0700

    Intent Services: Add two button to MainActivity.kt for start and stop intent service

--- intentservices/src/main/java/com/ehsankolivand/intentservices/MainActivity.kt
+++ intentservices/src/main/java/com/ehsankolivand/intentservices/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.ehsankolivand.intentservices
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.ehsankolivand.intentservices.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),View.OnClickListener {
     var activityMainBinding: ActivityMainBinding?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         val view = activityMainBinding!!.root
         setContentView(view)
+
+
+
+        activityMainBinding!!.btnStart.setOnClickListener(this)
+        activityMainBinding!!.btnStop.setOnClickListener(this)
+    }
+    override fun onClick(v: View?) {
+
+        when(v)
+        {
+            activityMainBinding!!.btnStart -> {
+
+            }
+            activityMainBinding!!.btnStop->{
+
+            }
+        }
     }
 }
