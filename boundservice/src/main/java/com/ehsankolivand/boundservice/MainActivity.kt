007257547e4af10085e808ae8a1ec539d5e2519f
ehsankolivand <ehsankolivandeh@gmail.com>
2021-05-11 06:07:32 -0700

    Bound Service: finally it's work

--- boundservice/src/main/java/com/ehsankolivand/boundservice/MainActivity.kt
+++ boundservice/src/main/java/com/ehsankolivand/boundservice/MainActivity.kt
@@ -7,35 +7,18 @@ import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
+import android.view.View
+import android.widget.Toast
 import com.ehsankolivand.boundservice.databinding.ActivityMainBinding
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private var activityMainBinding: ActivityMainBinding?=null
 
     private lateinit var  myBoundService:MyBoundService
 
     private var mBound: Boolean = false
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
-        activityMainBinding!!.root.apply {
-            setContentView(this)
-        }
-
-
-
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        bindService(Intent(this,MyBoundService::class.java)
-            ,serviceConnection,Context.BIND_AUTO_CREATE)
-
-    }
-    val serviceConnection = object :ServiceConnection{
+    private val serviceConnection = object :ServiceConnection{
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
             val binder = service as MyBoundService.LocalBinder
             myBoundService = binder.getService()
@@ -48,4 +31,38 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        activityMainBinding!!.root.apply {
+            setContentView(this)
+        }
+
+        activityMainBinding!!.button.setOnClickListener(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Intent(this, MyBoundService::class.java).also { intent ->
+            bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+        }
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unbindService(serviceConnection)
+        mBound = false
+    }
+
+
+    override fun onClick(v: View?) {
+        if (mBound)
+        activityMainBinding!!.button.text = myBoundService.randomNumber.toString()
+        else
+            Toast.makeText(this,"not bind",Toast.LENGTH_LONG).show()
+    }
+
+
 }
