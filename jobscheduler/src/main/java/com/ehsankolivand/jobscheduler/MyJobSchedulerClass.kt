1aef52c76fb8afcdd750fdfa3b522ceb7e3cbcfc
ehsankolivand <ehsankolivandeh@gmail.com>
2021-05-14 07:22:35 -0700

    JobScheduler : Create MyJobSchedulerClass and implement JobService Methods

--- jobscheduler/src/main/java/com/ehsankolivand/jobscheduler/MyJobSchedulerClass.kt
+++ jobscheduler/src/main/java/com/ehsankolivand/jobscheduler/MyJobSchedulerClass.kt
@@ -1,4 +1,16 @@
 package com.ehsankolivand.jobscheduler
 
-class MyJobSchedulerClass {
+import android.app.job.JobParameters
+import android.app.job.JobScheduler
+import android.app.job.JobService
+
+class MyJobSchedulerClass : JobService() {
+    override fun onStartJob(params: JobParameters?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        TODO("Not yet implemented")
+    }
+
 }
